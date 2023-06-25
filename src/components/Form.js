7a7b7edd88d19d6a73bf1a2ac25b3257d949642b
Bilal-hajM <bblal1003@gmail.com>
2023-06-25 22:44:31 +0300

    efswe

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,38 +1,59 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  
+  const [firstName, setFirstName] = useState("John");
+  const [lastName, setLastName] = useState("Henry");
+  const [submittedData, setSubmittedData] = useState([]);
+  const [errors, setErrors] = useState([]);
+
   function handleFirstNameChange(event) {
-    setFirstName(event.target.value)
+    setFirstName(event.target.value);
   }
 
-  const handleLastNameChange = (event) => {
-   console.log( setLastName(event.target.value))
+  function handleLastNameChange(event) {
+    setLastName(event.target.value);
   }
 
-  function DisplayDat() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    // first name is required
+    if (firstName.length > 0) {
+      const formData = { firstName: firstName, lastName: lastName };
+      const dataArray = [...submittedData, formData];
+      setSubmittedData(dataArray);
+      setFirstName("");
+      setLastName("");
+      setErrors([]);
+    } else {
+      setErrors(["First name is required!"]);
+    }
+  }
+
+  const listOfSubmissions = submittedData.map((data, index) => {
     return (
-      <div>
-        <h1>{firstName}</h1>
-        <h1>{lastName}</h1>
+      <div key={index}>
+        {data.firstName} {data.lastName}
       </div>
     );
-  }
+  });
 
   return (
-    <form>
-      <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
-      <input 
-      type="text" 
-      value={firstName} 
-      onChange={handleFirstNameChange} 
-      />
-      <input type="text" value={lastName} onChange={handleLastNameChange} />
-      <button type="submit">Submit</button>
-      <DisplayDat/>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleFirstNameChange} value={firstName} />
+        <input type="text" onChange={handleLastNameChange} value={lastName} />
+        <button type="submit">Submit</button>
+      </form>
+    {errors.length > 0
+      ? errors.map((error, index) => (
+          <p key={index} style={{ color: "red" }}>
+            {error}
+          </p>
+        ))
+      : null}
+    <h3>Submissions</h3>
+    {listOfSubmissions}
+    </div>
   );
 }
 
